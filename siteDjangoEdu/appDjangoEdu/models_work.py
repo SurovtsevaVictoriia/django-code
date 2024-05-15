44ce3b9116f123b6b89287ee123981fe411926ba
SurovtsevaVictoriia <surovtseva.vp@phystech.edu>
2024-05-15 14:23:49 +0300

    wonky get_themes_as_tree works

--- siteDjangoEdu/appDjangoEdu/models_work.py
+++ siteDjangoEdu/appDjangoEdu/models_work.py
@@ -1,4 +1,7 @@
 from .models import Questions, Themes
+# import sys
+
+# sys.setrecursionlimit(25)
 
 
 def get_subthemes(theme_id: int) -> list[int]:
@@ -14,7 +17,9 @@ def get_subthemes(theme_id: int) -> list[int]:
     children_ids = []
 
     def get_children(theme_ids):
-        # print('before before cycle', roots, children_ids, theme_ids)
+
+        print('before before cycle', id(roots),
+              id(children_ids), id(theme_ids))
         roots.extend(children_ids)
         children_ids.clear()
         # print('before cycle', roots, children_ids, theme_ids)
@@ -44,6 +49,45 @@ def get_subthemes(theme_id: int) -> list[int]:
     return roots
 
 
+def get_themes_as_tree() -> list:
+    """returns entire tree of theme ids as nested list
+
+    Returns:
+        list: nested list such as [0, [1, [3]], [2]]
+    """
+
+    roots = [0]
+    k = 0
+
+    def get_children(theme_ids: list, i):
+        i = i+1
+
+        print(i, 'roots', roots, 'theme_ids', theme_ids)
+        try:
+            if type(theme_ids[0]) == int:
+                print('in int if', theme_ids[0])
+                child_themes = Themes.objects.filter(parent_id=theme_ids[0])
+                for child_theme in child_themes:
+                    theme_ids.append([child_theme.theme_id])
+                get_children(theme_ids[1:], i)
+                # children_ids.append([child_theme.theme_id])
+            else:
+                for idx, theme_id in enumerate(theme_ids):
+                    print('in list if, idx = ', idx, 'theme_id ', theme_id)
+                    child_themes = Themes.objects.filter(
+                        parent_id=theme_ids[idx][0])
+                    for child_theme in child_themes:
+                        theme_ids[idx].append([child_theme.theme_id])
+                    get_children(theme_ids[idx][1:], i)
+                    # children_ids.append(child_theme.theme_id)
+
+        except Exception as e:
+            print(e.args, "get_children not getting children")
+
+    get_children(roots, k)
+    return roots
+
+
 def get_questions(theme_id: int) -> list[dict]:
     """returns questions on given theme and all the subthemes
     Args:
@@ -60,6 +104,3 @@ def get_questions(theme_id: int) -> list[dict]:
             questions.append(theme_q.get_as_dict())
 
     return questions
-
-
-
